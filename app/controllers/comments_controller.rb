d372f6e4fd8fda890ccab757a57ed6731dab50ad
psswf1172 <jennifermabella@gmail.com>
2018-05-26 16:05:14 -0400

    fixes comment id error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
     @comment = @recipe.comments.create(comment_params)
     redirect_to recipe_path(@recipe)
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
     @comment = @recipe.comments.find(params[:id])
     @comment.destroy
     redirect_to recipe_path(@recipe)
