a58247110263527f968453381981bd5d2d4fbb4b
psswf1172 <jennifermabella@gmail.com>
2018-06-08 13:40:04 -0400

    fixes config syntax

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,9 +4,15 @@ rails_env = Rails.env || 'development'
 redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
 redis_config.merge! redis_config.fetch(Rails.env, {})
 redis_config.symbolize_keys!
+# Sidekiq.configure_server do |config|
+#  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+# end
+# Sidekiq.configure_client do |config|
+#  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+# end
 Sidekiq.configure_server do |config|
- config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+ config.redis = { host: "#{redis_config[:host]}", port: "#{redis_config[:port]}", db: 12 }
 end
 Sidekiq.configure_client do |config|
- config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+ config.redis = { host: "#{redis_config[:host]}", port: "#{redis_config[:port]}", db: 12 }
 end
