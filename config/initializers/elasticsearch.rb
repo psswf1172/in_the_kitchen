8eaa5511ec3b704bd2d4624e27272cd75c824658
psswf1172 <jennifermabella@gmail.com>
2018-06-18 18:14:11 -0400

    uncommented elasticsearch bc should connect now

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,11 +1,11 @@
-# ENV["ELASTICSEARCH_URL"] = "https://search-in-the-monaco-kitchen-hog3n323ovglhuyra3ogmnuss4.us-east-1.es.amazonaws.com"
+ENV["ELASTICSEARCH_URL"] = "https://search-in-the-monaco-kitchen-hog3n323ovglhuyra3ogmnuss4.us-east-1.es.amazonaws.com"
 
-# # Elasticsearch::Model.client = Elasticsearch::Client.new({
-# #   log: true 
-# # })
+# Elasticsearch::Model.client = Elasticsearch::Client.new({
+#   log: true 
+# })
 
-# Searchkick.aws_credentials = {
-#   access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#   region: "us-east-1"
-# }
+Searchkick.aws_credentials = {
+  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+  region: "us-east-1"
+}
