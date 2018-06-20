2d4dc8447918b20899b482c3273701693fbd1b7c
psswf1172 <jennifermabella@gmail.com>
2018-06-20 18:16:17 -0400

    fixing elasticsearch config error

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,13 @@
-ENV["ELASTICSEARCH_URL"] = "https://search-in-the-monaco-kitchen-hog3n323ovglhuyra3ogmnuss4.us-east-1.es.amazonaws.com"
+# ENV["ELASTICSEARCH_URL"] = "https://search-in-the-monaco-kitchen-hog3n323ovglhuyra3ogmnuss4.us-east-1.es.amazonaws.com"
+
+config = {
+  transport_options: { request: { timeout: 5 } }
+}
 
 if File.exists?("config/elasticsearch.yml")
-   config = YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys
-   Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+  template = ERB.new(File.new("config/elasticsearch.yml").read)
+  processed = YAML.load(template.result(binding))
+  config.merge!(processed[Rails.env].symbolize_keys)
 end
 
+Elasticsearch::Model.client = Elasticsearch::Client.new(config)
