57cd51c629992f9cc39363443db93c8e7d6e6f3c
psswf1172 <jennifermabella@gmail.com>
2018-06-22 10:23:11 -0400

    uggg

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,10 +4,28 @@
 
 # Elasticsearch::Model.client = Elasticsearch::Client.new(host: ELASTICSEARCH_URL)
 
-Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
+# Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
+#   f.request :aws_signers_v4,
+#             credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+#             service_name: 'es',
+#             region: 'us-east-1'
+#   f.adapter Faraday.default_adapter
+# end
+
+require 'faraday_middleware/aws_signers_v4'
+
+transport_configuration = lambda do |f|
   f.request :aws_signers_v4,
-            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-            service_name: 'es',
-            region: 'us-east-1'
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+    service_name: 'es',
+    region: 'us-east-1'
+  
+  f.response :logger
   f.adapter Faraday.default_adapter
 end
+
+transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new \
+  hosts: [{ scheme: 'https', host: ENV['ELASTICSEARCH_URL'], port: '443' }],
+  &transport_configuration
+
+Elasticsearch::Model.client = Elasticsearch::Client.new transport: transport
