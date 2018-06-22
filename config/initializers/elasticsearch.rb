6d85ba5885a6c8dcbd693835c4cb6d45573a842a
psswf1172 <jennifermabella@gmail.com>
2018-06-22 13:44:25 -0400

    new way to organize faraday connectin

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,23 +1,35 @@
-# ELASTICSEARCH_URL = ENV["ELASTICSEARCH_URL"] || 'http://localhost:9200'
-
-# Elasticsearch::Model.client = Elasticsearch::Client.new(host: ELASTICSEARCH_URL)
-
 require 'faraday_middleware'
 require 'faraday_middleware/aws_sigv4'
-require 'pp'
 
-conn = Faraday.new(url: 'https://search-in-the-monaco-kitchen-hog3n323ovglhuyra3ogmnuss4.us-east-1.es.amazonaws.com') do |faraday|
+conn = Faraday.new(url: 'https://search-in-the-monaco-kitchen-hog3n323ovglhuyra3ogmnuss4.us-east-1.es.amazonaws.com') do | faraday |
   faraday.request :aws_sigv4,
-    service: 'apigateway',
+    service: 'es',
     region: 'us-east-1',
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   # see http://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/Sigv4/Signer.html
 
-  faraday.response :json, content_type: /\bjson\b/
-  faraday.response :raise_error
-
   faraday.adapter Faraday.default_adapter
 end
 
-# res = conn.get '/'
+# faraday_config = lambda do |faraday|
+#   faraday.request :aws_sigv4, {
+#     credentials: Aws::Credentials.new(
+#       ENV["AWS_ACCESS_KEY_ID"],
+#       ENV["AWS_SECRET_ACCESS_KEY"]
+#       ),
+#       service_name: "es",
+#       region: 'us-east-1'
+#     }
+#     faraday.adapter :typhoeus
+#   end
+
+# elasticsearch_host_config = {
+#   host:   ENV["ELASTICSEARCH_HOST"],
+#   port:   ENV["ELASTICSEARCH_PORT"],
+#   scheme: ENV["ELASTICSEARCH_SCHEME"]
+# }
+
+# transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new(hosts: [elasticsearch_host_config], &faraday_config)
+
+# client = Elasticsearch::Client.new(transport: transport)
