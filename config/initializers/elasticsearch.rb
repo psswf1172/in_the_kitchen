e72b1ec67cdd39a67e6d876de158b3911d99c605
psswf1172 <jennifermabella@gmail.com>
2018-06-22 10:47:49 -0400

    back to the gd beginning

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,17 +1,17 @@
 # require 'faraday_middleware'
 
-# ELASTICSEARCH_URL = ENV["ELASTICSEARCH_URL"] || 'http://localhost:9200'
+ELASTICSEARCH_URL = ENV["ELASTICSEARCH_URL"] || 'http://localhost:9200'
 
-# Elasticsearch::Model.client = Elasticsearch::Client.new(host: ELASTICSEARCH_URL)
+Elasticsearch::Model.client = Elasticsearch::Client.new(host: ELASTICSEARCH_URL)
 
-require 'faraday_middleware/aws_sigv4'
-Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
-  f.request :aws_signers_v4,
-            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-            service_name: 'es',
-            region: 'us-east-1'
-  f.adapter Faraday.default_adapter
-end
+# require 'faraday_middleware/aws_sigv4'
+# Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
+#   f.request :aws_signers_v4,
+#             credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+#             service_name: 'es',
+#             region: 'us-east-1'
+#   f.adapter Faraday.default_adapter
+# end
 
 
 # transport_configuration = lambda do |f|
