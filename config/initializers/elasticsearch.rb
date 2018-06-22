c8ea5f5e0a1747aabc4b8d41342fb1991444822e
psswf1172 <jennifermabella@gmail.com>
2018-06-22 10:39:24 -0400

    oi

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,31 +1,31 @@
-require 'faraday_middleware'
+# require 'faraday_middleware'
 
 # ELASTICSEARCH_URL = ENV["ELASTICSEARCH_URL"] || 'http://localhost:9200'
 
 # Elasticsearch::Model.client = Elasticsearch::Client.new(host: ELASTICSEARCH_URL)
 
-# Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
-#   f.request :aws_signers_v4,
-#             credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-#             service_name: 'es',
-#             region: 'us-east-1'
-#   f.adapter Faraday.default_adapter
-# end
-
 require 'faraday_middleware/aws_sigv4'
-
-transport_configuration = lambda do |f|
+Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
   f.request :aws_signers_v4,
-    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-    service_name: 'es',
-    region: 'us-east-1'
-  
-  f.response :logger
+            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+            service_name: 'es',
+            region: 'us-east-1'
   f.adapter Faraday.default_adapter
 end
 
-transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new \
-  hosts: [{ scheme: 'https', host: ENV['ELASTICSEARCH_URL'], port: '443' }],
-  &transport_configuration
 
-Elasticsearch::Model.client = Elasticsearch::Client.new transport: transport
+# transport_configuration = lambda do |f|
+#   f.request :aws_signers_v4,
+#     credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+#     service_name: 'es',
+#     region: 'us-east-1'
+  
+#   f.response :logger
+#   f.adapter Faraday.default_adapter
+# end
+
+# transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new \
+#   hosts: [{ scheme: 'https', host: ENV['ELASTICSEARCH_URL'], port: '443' }],
+#   &transport_configuration
+
+# Elasticsearch::Model.client = Elasticsearch::Client.new transport: transport
