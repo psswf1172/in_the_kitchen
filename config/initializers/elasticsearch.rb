4c18496d83b7fe71b39d20482de1f7ef2e4071a0
psswf1172 <jennifermabella@gmail.com>
2018-06-24 15:43:38 -0400

    updated access policy

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,9 +3,17 @@ require 'faraday_middleware/aws_sigv4'
 
 ENV["ELASTICSEARCH_URL"] || "localhost:9200"
 
-Searchkick.aws_credentials = {
-  access_key_id: ENV["AWS_ACCESS_KEY_ID"], 
-  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-  service: "es",
-  region: "us-east-1"
-}
+Searchkick.client = Elasticsearch::Client.new(url: ENV["ELASTICSEARCH_URL"] || "localhost:9200") do | f |
+  f.request :aws_sigv4,
+    service: "es",
+    region: "us-east-1",
+    credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
+end
+
+
+# Searchkick.aws_credentials = {
+#   access_key_id: ENV["AWS_ACCESS_KEY_ID"], 
+#   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#   service: "es",
+#   region: "us-east-1"
+# }
